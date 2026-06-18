2fa6f609e8a31f760871025921950da86f7d3e1c
Mithil Damani <mithildamani@Mithils-MacBook-Air.local>
2026-06-18 18:42:25 -0400

    feat: add transfer route and navigation

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { AccountsModule } from './features/accounts/accounts-module';
+import { TransfersModule } from './features/transfers/transfers-module';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AccountsModule, AppRoutingModule],
+  imports: [BrowserModule, AccountsModule, TransfersModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AccountCreationComponent } from './features/accounts/account-creation/account-creation';
+import { FundTransferComponent } from './features/transfers/fund-transfer/fund-transfer';
 
 const routes: Routes = [
   {
     path: 'accounts/new',
     component: AccountCreationComponent,
     title: 'Create account | ClearBank',
+  },
+  {
+    path: 'transfers',
+    component: FundTransferComponent,
+    title: 'Transfer funds | ClearBank',
   },
   {
     path: '',
